4d2d4a9391337e37802a3f5abfe7c0bddcfbc8d7
Max Gilbert <gilbertlmax@gmail.com>
2012-09-15 11:44:30 -0400

    intersection and weirdness

--- assets/js/Squirrel.js
+++ assets/js/Squirrel.js
@@ -19,4 +19,27 @@ Squirrel.prototype.move = function() {
 		this.bounds.y += this.velocity.y;
 		this.bounds.z += this.velocity.x;
 		this.bounds.w += this.velocity.y;
-	}
+	};
+Squirrel.prototype.intersectPlatform = function(object) {
+    //console.log('why');
+    if(this.velocity.y>0){   //object.platform
+        return 'NONE';
+    }else if (this.bounds.w - this.velocity.y < object.bounds.y + object.velocity.y){
+        return 'NONE';
+    }else if (this.bounds.z + this.velocity.x<object.bounds.x + object.velocity.x||
+        this.bounds.w + this.velocity.y>object.bounds.y + object.velocity.y||
+        this.bounds.x + this.velocity.x>object.bounds.z + object.velocity.x||
+        this.bounds.y + this.velocity.y<object.bounds.w + object.velocity.y) {
+        return 'NONE';
+    } else if ( this.bounds.z < object.bounds.x && this.bounds.z + this.velocity.x >= object.bounds.x + object.velocity.x ) {
+        return 'RIGHT';
+    } else if ( this.bounds.w > object.bounds.y && this.bounds.w + this.velocity.y <= object.bounds.y + object.velocity.y ){
+        return 'DOWN';
+    } else if ( this.bounds.x > object.bounds.z && this.bounds.x + this.velocity.x <= object.bounds.z + object.velocity.x ){
+        return 'LEFT';
+    } else if ( this.bounds.y < object.bounds.w && this.bounds.y + this.velocity.y >= object.bounds.w + object.velocity.y ){
+        return 'TOP';
+    } else {
+        return 'MIDDLE';
+    }
+};
