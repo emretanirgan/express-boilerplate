de2649b9d368305b89c2895cd17b5cc4f4c48e5e
Emre Tanirgan <emretanirgan@hotmail.com>
2012-09-16 11:27:20 +0300

    stuff

--- assets/js/Textures.js
+++ assets/js/Textures.js
@@ -6,23 +6,21 @@ Textures = {
 	'hotdog' : {src:'img/hotdog.png', map:undefined},
 	'pizza' : {src:'img/pizza.png', map:undefined},
 	'toast' : {src:'img/toast.png', map:undefined},
-	'rat' : {src:'img/rat.png', map:undefined}
-	'theoone' : {src: 'img/theo1.png', map:undefined},
-	'theotwo' : {src: 'img/theo2.png', map:undefined},
-	'theothree' : {src: 'img/theo3.png', map:undefined},
-	'theofour' : {src: 'img/theo4.png', map:undefined},
-    /*'maxone' : {src: '', map:undefined},
-	'maxtwo' : {src: '', map:undefined},
-	'maxthree' : {src: '', map:undefined},
-	'maxfour' : {src: '', map:undefined},
-	'emreone' : {src: '', map:undefined},
-	'emretwo' : {src: '', map:undefined},
-	'emrethree' : {src: '', map:undefined},
-	'emrefour' : {src: '', map:undefined},
-	'background' : {src: '', map:undefined},
-	'tree1' : {src: '', map:undefined},
-	'tree2' : {src: '', map:undefined}*/
-
+	'rat' : {src:'img/rat.png', map:undefined},
+	'background' : {src:'img/background.png', map:undefined},
+	'collegehall' : {src:'img/collegehall.png', map:undefined},
+	'emre1' : {src:'img/emre1.png', map:undefined},
+	'emre2' : {src:'img/emre2.png', map:undefined},
+	'emre3' : {src:'img/emre3.png', map:undefined},
+	'emre4' : {src:'img/emre4.png', map:undefined},
+	'theo1' : {src:'img/theo1.png', map:undefined},
+	'theo2' : {src:'img/theo2.png', map:undefined},
+	'theo3' : {src:'img/theo3.png', map:undefined},
+	'theo4' : {src:'img/theo4.png', map:undefined},
+	'max1' : {src:'img/max1.png', map:undefined},
+	'max1' : {src:'img/max1.png', map:undefined},
+	'max1' : {src:'img/max1.png', map:undefined},
+	'max1' : {src:'img/max1.png', map:undefined}
 }
 
 preLoadTextures = function() {
